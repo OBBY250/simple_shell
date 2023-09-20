773bda563a5d8104d6c7a3a511da0dd9c8236b10
Joseph-Oje <josephoje247@gmail.com>
2023-09-20 15:06:56 -0700

    shell

---
+++ shell.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	const char *str = "/bin/ls"
+		for (int i = 0; str[i] != '\0'; i++)
+		{
+			putchar(str[i]);
+		}
+	putchar('\n');
+	return 0;
+}
