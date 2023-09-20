98cd754749017497959d3585eeadd7e0e2360c48
Joseph-Oje <josephoje247@gmail.com>
2023-09-20 15:39:51 -0700

    shell

--- shell.c
+++ shell.c
@@ -1,5 +1,7 @@
 #include "shell.h"
 
+#define MAX_INPUT_LENGTH 1024
+
 /**
  * main - Entry point of the program.
  *
@@ -7,12 +9,27 @@
  */
 int main(void)
 {
-	const char *str = "/bin/ls"
-	int i;
-		for (i = 0; str[i] != '\0'; i++)
+	char input[MAX_INPUT_LENGTH];
+
+	while (1)
+	{
+		printf("($) ");
+		if (fgets(input, sizeof(input), stdin) == NULL)
 		{
-			putchar(str[i]);
+			break;
 		}
-	putchar('\n');
+
+		size_t len = strlen(input);
+
+		if (len > 0 && input[len - 1] == '\n')
+		{
+			input[len - 1] = '\0';
+		}
+		if (strcmp(input, "exit") == 0)
+		{
+			break;
+		}
+		system(input);
+	}
 	return (0);
 }
